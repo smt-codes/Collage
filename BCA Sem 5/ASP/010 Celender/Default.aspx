802fa5a0686495813580de54566b35abf091fee3
smt-codes <smtcodes@gmail.com>
2020-09-19 00:43:27 +0530

    ASP 010 Celender

---
+++ BCA Sem 5/ASP/010 Celender/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <br /><br />
+    <div>
+        <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+
+        <br /><br />
+        <asp:Button ID="btn_select" runat="server" Text="Show Date Details" OnClick="btn_select_Click"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ BCA Sem 5/ASP/010 Celender/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn_select_Click(object sender, EventArgs e)
+    {
+        DateTime d = Calendar1.SelectedDate;
+        Response.Write("Date : " + d.ToString());
+        Response.Write("<br>Short Date : " + d.ToShortDateString() + ", Long Date : " + d.ToLongDateString());
+        Response.Write("<br>Day : " + d.Day + ", Month : " + d.Month + ", Year : " + d.Year);
+        Response.Write("<br>Day of Week : " + d.DayOfWeek + ", Day of Year : " + d.DayOfYear);
+    }
+}

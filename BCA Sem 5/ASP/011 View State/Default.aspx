043047c5d2d9136e2d83503c1e5ba17970c3281c
smt-codes <smtcodes@gmail.com>
2020-09-19 00:43:51 +0530

    ASP 011 View State

---
+++ BCA Sem 5/ASP/011 View State/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" EnableViewState="false" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <br /><br />
+        View State Example
+        <br /><br />
+        <asp:Label ID="lbl_count" runat="server"></asp:Label>
+        <br /><br />
+        <asp:Button ID="btn_add" runat="server" Text="Add Count" OnClick="btn_add_Click"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ BCA Sem 5/ASP/011 View State/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("View State Status : " + Page.EnableViewState.ToString());
+        lbl_count.Text = "Page Count : " + counter.ToString();
+    }
+    public int counter
+    {
+        get
+        {
+            if(ViewState["count"] != null)
+            {
+                return ((int)ViewState["count"]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        set
+        {
+            ViewState["count"] = value;
+        }
+    }
+    protected void btn_add_Click(object sender, EventArgs e)
+    {
+        counter++;
+        lbl_count.Text = "Page Count : " + counter.ToString();
+    }
+}

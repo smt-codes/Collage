d08fb0b18263f747e8250504346d2cf136c16982
smt-codes <smtcodes@gmail.com>
2020-09-19 00:42:56 +0530

    ASP 009 File Upload

---
+++ BCA Sem 5/ASP/009 File Upload/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <br /><br />
+        <div>
+            <label>Select File</label>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+        </div>
+        <br /><br />
+        <div>
+            <asp:Button ID="btn_upload" runat="server" Text="Upload My File" OnClick="btn_upload_Click" />
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ BCA Sem 5/ASP/009 File Upload/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn_upload_Click(object sender, EventArgs e)
+    {
+        if(FileUpload1.HasFile){
+            Response.Write("<h2> File Details</h2>");
+            Response.Write("<br> File Name : " + FileUpload1.FileName);
+            Response.Write("<br> Has File : " + FileUpload1.HasFile);
+            Response.Write("<br> Length : " + FileUpload1.FileBytes.Length);
+            Response.Write("<br> Read Only : " + FileUpload1.FileBytes.IsReadOnly);
+
+            HttpPostedFile pfile = FileUpload1.PostedFile;
+            Response.Write("<br> Posted File Name : " + pfile.FileName);
+            Response.Write("<br> Posted File Content Type : " + pfile.ContentType);
+            Response.Write("<br> Posted File Content Lnegth : " + pfile.ContentLength);
+
+            string PhysicalPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = System.IO.Path.GetFileName(FileUpload1.FileName);
+            FileUpload1.SaveAs(PhysicalPath + "/uploads/" + FileName);
+            Response.Write("<h2>File " + FileName + " Uploaded</h2>");
+        }
+        else
+        {
+            Response.Write("<h2>No File Uploaded</h2>");
+        }
+    }
+}
